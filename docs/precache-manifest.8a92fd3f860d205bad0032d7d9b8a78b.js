7ea1806706ece852d577703c8611b52818bf2dc9
Ray Hogan <rayhogan@gmail.com>
2020-08-28 19:13:22 +0100

    Updating build to allow for github page publishing

---
+++ docs/precache-manifest.8a92fd3f860d205bad0032d7d9b8a78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db27f2a7580b9b8fa482f6a744a8e98",
+    "url": "/oeuf/index.html"
+  },
+  {
+    "revision": "8c27bd7758ed5df31142",
+    "url": "/oeuf/static/css/main.21d0b8b0.chunk.css"
+  },
+  {
+    "revision": "7642f34a7a11b98ec360",
+    "url": "/oeuf/static/js/2.b8fd3db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oeuf/static/js/2.b8fd3db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c27bd7758ed5df31142",
+    "url": "/oeuf/static/js/main.03beb5ae.chunk.js"
+  },
+  {
+    "revision": "41056804f780600ca177",
+    "url": "/oeuf/static/js/runtime-main.ef1e3f1a.js"
+  },
+  {
+    "revision": "80659242841dee9a587db829f0360dbe",
+    "url": "/oeuf/static/media/egg.80659242.svg"
+  }
+]);
