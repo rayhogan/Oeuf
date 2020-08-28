67c27689159cdf6f1e7826257c7cf6a6ecbbaca6
Ray Hogan <rayhogan@gmail.com>
2020-08-28 19:46:36 +0100

    Another build

---
+++ docs/precache-manifest.c33779a9f9586db3b90717e50e0a35fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d901e38551f40c44be1e0a627e892a64",
+    "url": "/Oeuf/index.html"
+  },
+  {
+    "revision": "8323f791e0c29a8d3d6c",
+    "url": "/Oeuf/static/css/main.21d0b8b0.chunk.css"
+  },
+  {
+    "revision": "c8fc97d771e8a3041546",
+    "url": "/Oeuf/static/js/2.70a6db46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Oeuf/static/js/2.70a6db46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8323f791e0c29a8d3d6c",
+    "url": "/Oeuf/static/js/main.f3653f54.chunk.js"
+  },
+  {
+    "revision": "fef48c4de212cdeba927",
+    "url": "/Oeuf/static/js/runtime-main.a8182d57.js"
+  },
+  {
+    "revision": "80659242841dee9a587db829f0360dbe",
+    "url": "/Oeuf/static/media/egg.80659242.svg"
+  }
+]);
